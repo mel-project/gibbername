8ff5d83ca17767a0496a5e926b67a74d9db2b3e1
sadministrator <marcothewebdev@gmail.com>
2023-02-21 23:27:39 -0600

    Encode/decode functions and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use themelio_structs::BlockHeight;
+
+/// Decodes a gibbername into a blockchain location.
+fn decode_gibbername(gname: &str) -> anyhow::Result<(BlockHeight, u32)> {
+    let (height, index) = gibbercode::decode(gname);
+    Ok((BlockHeight(height as u64), index as u32))
+}
+
+/// Encodes a blockchain location into a gibbername.
+fn encode_gibbername(height: BlockHeight, index: u32) -> String {
+    // get the u64 out of the BlockHeight
+    gibbercode::encode(height.0 as u128, index as u128)
 }
 
 #[cfg(test)]
@@ -7,8 +17,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn encodes_gibbername() {
+        let height = BlockHeight(216);
+        let index = 2;
+        let gname = encode_gibbername(height, index);
+
+        assert_eq!(gname, "lol".to_string());
+    }
+
+    #[test]
+    fn decodes_gibbername() {
+        let gname = "hehe-lol";
+        let (height, index) = gibbercode::decode(gname);
+
+        assert_eq!(height, 216);
+        assert_eq!(index, 2);
     }
 }
